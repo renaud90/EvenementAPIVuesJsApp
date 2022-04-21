5f2c483ff3fe58331391321aa4a646941a5bd899
Mohammed Jalaleddine <mojae1985@gmail.com>
2022-04-21 10:47:53 -0400

    Ajout de page acceuil

--- src/vue-evenements-app/src/layouts/BaseLayout.vue
+++ src/vue-evenements-app/src/layouts/BaseLayout.vue
@@ -1,21 +1,24 @@
 <template>
-    <div class="container">
-        <header>
-            <slot name="header">
-                <nav>
-                    <router-link to="/">Home</router-link> |
-                    <router-link to="/evenements">Evenements</router-link> |
-                    <router-link id="login" to="/login">Login</router-link>
-                </nav>
-            </slot>
-        </header>
-        <main>
-            <slot></slot>
-        </main>
-        <footer id="footer">
-            <slot name="footer">
-                dafult footer content
-            </slot>
-        </footer>
-    </div>
+  <div class="container">
+    <header>
+      <slot name="header">
+        <nav>
+          <router-link to="/">Home</router-link> |
+          <router-link to="/evenements">Evenements</router-link> |
+          <router-link id="login" to="/login">Login</router-link>
+        </nav>
+      </slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer id="footer">
+      <slot name="footer">
+        <h3>
+          Ce projet a été Réalisé dans le cadre du cours dévelppement web 2 au
+          cégep Limoilou.
+        </h3>
+      </slot>
+    </footer>
+  </div>
 </template>
